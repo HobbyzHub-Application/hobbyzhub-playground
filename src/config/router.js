7d0f301f25f9e89217aa673ffbcccf3ec74807c8
Q-T5 <bikathimartin@outlook.com>
2023-05-11 21:07:15 +0300

    Refactor and introduce new routes

--- src/config/router.js
+++ src/config/router.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import GetStartedView from "../views/GetStartedView.vue"
+import GetStartedView from "../views/GetStartedView.vue";
+import SignInView from "../views/SignInView.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: "/get-started/",
-            component: GetStartedView,
-            name: "GetStarted"
-        }
-    ]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/hobbyzhub-playground/",
+      component: SignInView,
+      name: "GetStarted",
+    },
+    {
+      path: "/hobbyzhub-playground/get-started/",
+      component: GetStartedView,
+      name: "GetStarted",
+    },
+  ],
 });
 
 export default router;
