843a9701da39e21fd92b30747f7564df551e2491
Q-T5 <bikathimartin@outlook.com>
2023-05-11 21:07:56 +0300

    Create two new views for the app

---
+++ src/views/GetStartedView.vue
@@ -0,0 +1,11 @@
+<script lang="js">
+  export default {
+    name: "GetStartedView"
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Getting starte view...</h1>
+  </div>
+</template>
---
+++ src/views/SignInView.vue
@@ -0,0 +1,11 @@
+<script lang="js">
+export default {
+    name: "SignInView"
+}
+</script>
+
+<template>
+    <div>
+        <h1>Sign In</h1>
+    </div>
+</template>
