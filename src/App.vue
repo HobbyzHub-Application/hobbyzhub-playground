71020fb1a14faf75e9ba8613fea15c3a33018d04
Q-T5 <bikathimartin@outlook.com>
2023-05-11 10:35:51 +0300

    Add basic structure for app with tabs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="js">
+import { ref } from 'vue';
+
+export default {
+  name: "App",
+  setup() {
+    const tabs = ref(null);
+
+    return {
+      tabs
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-screen w-screen">
+    <div class="h-[8%] shadow-md">
+      <v-tabs 
+        v-model="tabs"
+        fixed-tabs>
+        <v-tab value="followers">Followers & Followings</v-tab>
+        <v-tab value="categories">Hobby Categories</v-tab>
+        <v-tab value="chats">App Chats</v-tab>
+      </v-tabs>
+    </div>
+    <div class="h-[92%] rounded-md shadow-lg p-2 border-[1px] border-primary">
+      <v-window v-model="tabs">
+        <v-window-item value="followers">One</v-window-item>
+        <v-window-item value="categories">Two</v-window-item>
+        <v-window-item value="chats">Three</v-window-item>
+      </v-window>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  html {
+    overflow: auto !important;
+  }
 </style>
