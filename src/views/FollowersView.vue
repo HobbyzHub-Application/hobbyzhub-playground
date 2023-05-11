03d5f5d7edaea51c1bb7d552f48e958470b56e1f
Q-T5 <bikathimartin@outlook.com>
2023-05-11 11:31:13 +0300

    Add code for placeholding

--- src/views/FollowersView.vue
+++ src/views/FollowersView.vue
@@ -5,7 +5,13 @@
 </script>
 
 <template>
-  <div>
-    <h1>Followers View</h1>
+  <div class="h-full flex justify-center items-center">
+    <h1 class="inline-flex flex-col">
+      <img
+        src="../assets/under-construction.svg"
+        alt="under-construction"
+        class="h-24 mb-2" />
+      <span class="text-xl font-abeezee">Section Under Construction</span>
+    </h1>
   </div>
 </template>
