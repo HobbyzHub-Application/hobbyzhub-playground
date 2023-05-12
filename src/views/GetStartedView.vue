42ad5a9eed0e95fdb76840b19ac402bf5cf6a00c
Q-T5 <bikathimartin@outlook.com>
2023-05-12 12:24:55 +0300

    Fix issue with signup CORS

--- src/views/GetStartedView.vue
+++ src/views/GetStartedView.vue
@@ -6,23 +6,53 @@
       setup() {
         const signupLoading = ref(false);
         const newPasswordShow = ref(false);
+        const disableSignupButton = ref(true);
+        const signupSuccess = ref(false);
 
         // signup details
         const signupDetails = ref({
           email: "",
           password: ""
         });
-        const agreeCheckbox = ref(false);
 
-        // watch inputs to enable signup button
-        watch()
+        // watch inputs to enable or disable signup button
+        watch([
+          () => signupDetails.value.email,
+          () => signupDetails.value.password
+        ], (newValues) => {
+          if(newValues[0] !== "" && newValues[1] !== "") {
+            disableSignupButton.value = false;
+          } else {
+            disableSignupButton.value = true;
+          }
+        });
 
         function signup() {
-          // TODO : Add logic here
+          signupLoading.value = true;
+          // code to signup the user with API an call
+          fetch("http://149.28.232.132:8765/accountmanagement-service/api/auth/register", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(signupDetails.value)
+          }).then((response) => {
+            if(response.status !== 200) {
+              console.log("Error from signup response...");
+              signupLoading.value = false;
+            }
+
+            signupSuccess.value = true;
+            return response.json();
+          }).then((data) => {
+            console.log(`Response Data: ${ data }`);
+          }).catch((error) => {
+            console.log("Error occured: " + error);
+          });
         }
 
         return {
-          signupLoading, newPasswordShow, signup, signupDetails, agreeCheckbox
+          signupLoading, newPasswordShow, signup, signupDetails, disableSignupButton, signupSuccess
         }
       }
     }
@@ -30,7 +60,7 @@
 
 <template>
   <div class="h-full flex justify-center items-center">
-    <v-card class="w-[40%] h-[60%]">
+    <v-card class="w-[40%] h-[47%]">
       <v-card-title>Sign Up</v-card-title>
       <v-card-subtitle
         class="text-primary underline underline-offset-2 hover:cursor-pointer"
@@ -51,16 +81,14 @@
           @click:append-inner="newPasswordShow = !newPasswordShow"
           v-model="signupDetails.password"></v-text-field>
         <div class="flex justify-center flex-col items-center">
-          <v-checkbox
-            color="secondary"
-            label="I agree to site terms and conditions"
-            v-model="agreeCheckbox"></v-checkbox>
           <v-btn
             color="success"
             variant="flat"
             :loading="signupLoading"
-            disabled>
-            Sign Up
+            :disabled="disableSignupButton"
+            @click="signup"
+            class="w-[40%]">
+            Signup
           </v-btn>
         </div>
       </v-container>
