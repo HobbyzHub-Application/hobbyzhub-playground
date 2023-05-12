a366c9118b05fd6d471d38bc3be91ac315526155
Q-T5 <bikathimartin@outlook.com>
2023-05-12 10:44:46 +0300

    Replace previous look wiith router view

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,22 @@
 <script lang="js">
-  import { ref } from 'vue';
-  import CategoriesView from './views/CategoriesView.vue';
-  import FollowersView from './views/FollowersView.vue';
-  import ChatsView from './views/ChatsView.vue';
 
   export default {
     name: "App",
-    components: {
-      FollowersView, CategoriesView, ChatsView
-    },
     setup() {
-      const tabs = ref(null);
 
-      return {
-        tabs
-      }
     }
   }
 </script>
 
 <template>
   <div class="h-screen w-screen">
-    <div class="h-[8%] shadow-md">
-      <v-tabs
-        v-model="tabs"
-        fixed-tabs
-        class="font-abeezee">
-        <v-tab value="followers">Followers & Followings</v-tab>
-        <v-tab value="categories">Hobby Categories</v-tab>
-        <v-tab value="chats">App Chats</v-tab>
-      </v-tabs>
-    </div>
+    <div class="h-[8%] shadow-md"></div>
     <div class="h-[92%] rounded-md shadow-lg p-2 border-[1px] border-primary">
-      <v-window
-        v-model="tabs"
-        class="h-full">
-        <v-window-item
-          value="followers"
-          class="h-full">
-          <FollowersView />
-        </v-window-item>
-        <v-window-item
-          value="categories"
-          class="h-full">
-          <CategoriesView />
-        </v-window-item>
-        <v-window-item
-          value="chats"
-          class="h-full">
-          <ChatsView />
-        </v-window-item>
-      </v-window>
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
     </div>
   </div>
 </template>
