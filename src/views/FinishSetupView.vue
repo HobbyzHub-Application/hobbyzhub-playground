75c2a5851493f78e9b0a0835fb416ba64ca22a64
Q-T5 <bikathimartin@outlook.com>
2023-05-13 15:15:53 +0300

    Add new views for once logged in

---
+++ src/views/FinishSetupView.vue
@@ -0,0 +1,14 @@
+<script lang="js">
+export default {
+    name: "FinishSetup",
+    setup() {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Finish Setup</h1>
+    </div>
+</template>
---
+++ src/views/HobbyzhubPlayView.vue
@@ -0,0 +1,14 @@
+<script lang="js">
+export default {
+    name: "HobbyzhubPlayView",
+    setup() {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Play Ground View</h1>
+    </div>
+</template>
