081277560e0bdf398693014c19ad6ab92a381196
Q-T5 <bikathimartin@outlook.com>
2023-05-13 15:14:34 +0300

    Create app top-bar component

---
+++ src/components/TopBarComp.vue
@@ -0,0 +1,20 @@
+<script lang="js">
+    export default {
+        name: "TopBarComp",
+        setup() {
+
+        }
+    }
+</script>
+
+<template>
+    <div class="h-[8%] shadow-md p-2 flex items-center">
+        <h1 class="inline-flex items-center space-x-2">
+            <span class="font-abeezee text-lg">Hobbyzhub Playground</span>
+            <img
+                src="../assets/playground-logo.svg"
+                alt="playground-logo"
+                class="w-8" />
+        </h1>
+    </div>
+</template>
