12d363a59a329c93fe34f92002acc600cfe7045c
Q-T5 <bikathimartin@outlook.com>
2023-05-13 11:27:41 +0300

    Implement a logged-in user store

---
+++ src/stores/currentuser-store.js
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia";
+import { reactive, computed } from "vue";
+
+const useCurrentUserStore = defineStore("CurrentUserStore", () => {
+    // state
+    const currentUserData = reactive({
+        userName: "",
+        userEmail: "martbikathi@gmail.com",
+        userToken: "",
+        userProfilePicture: "",
+    });
+
+    // getters
+    const getCurrentUserEmail = computed(() => currentUserData.userEmail);
+
+    // setters
+    function setUserName(username) {
+        currentUserData.userName = username;
+    }
+
+    function setUserEmail(email) {
+        currentUserData.userEmail = email;
+    }
+
+    return {
+        getCurrentUserEmail,
+        setUserName,
+        setUserEmail,
+    };
+});
+
+export default useCurrentUserStore;
