3762692991b51a0967cfc3d971cb7d8d115d9b0e
Q-T5 <bikathimartin@outlook.com>
2023-05-16 15:34:50 +0300

    Add component to manage file picking

---
+++ src/components/FilePickerComp.vue
@@ -0,0 +1,14 @@
+<script lang="js">
+    export default {
+        name: "FilePickerComp",
+        setup() {
+
+        }
+    }
+</script>
+
+<template>
+    <div class="bg-red-500 flex justify-center p-2">
+        <h1>Profile picture selection</h1>
+    </div>
+</template>
